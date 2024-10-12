c6a6c912d86f05009bf90d1fcc8c0b3991181376
lifelongCat <lifelongcat@yandex.ru>
2024-10-12 13:50:58 +0300

    fix alembic upgrade error (move comment)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,3 @@
-# apply migrations
-alembic upgrade head
+alembic upgrade head # apply migrations
 # start fastapi app
 gunicorn app.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind="0.0.0.0:${FASTAPI_PORT}"
