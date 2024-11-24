66ae06647ea30d8190d21d8cbf2804ce13be74a6
lifelongCat <lifelongcat@yandex.ru>
2024-11-24 21:33:49 +0300

    add metrics with prometheus

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI, Request, Response
 from fastapi.middleware.cors import CORSMiddleware
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from app.config import settings
 from app.logger import logger
@@ -7,7 +10,13 @@ from app.routers.quizes import router as quizes_router
 from app.routers.users import router as users_router
 
 
-app = FastAPI(root_path='/api')
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    instrumentator.expose(app, tags=['metrics'])
+    yield
+
+
+app = FastAPI(root_path='/api', lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[f'http://localhost:{settings.FRONTEND_PORT}', 'http://localhost'],
@@ -15,6 +24,12 @@ app.add_middleware(
     allow_methods=['*'],
     allow_headers=['*'],
 )
+
+
+instrumentator = Instrumentator(
+    should_group_status_codes=False,
+    excluded_handlers=['/metrics', '/docs', '/openapi.json']
+).instrument(app, latency_lowr_buckets=(0.1, 0.25, 0.5, 0.75, 1))
 
 
 @app.middleware('http')
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,6 @@
 alembic upgrade head # apply migrations
+# variable for correct multiworkers gunicorn metrics scrapping
+mkdir -p prometheus-multiprocess
+export PROMETHEUS_MULTIPROC_DIR=$(pwd)/prometheus-multiprocess
 # start fastapi app
 gunicorn app.main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind="0.0.0.0:${FASTAPI_PORT}"
